68963757896c3e41757e71596a30c0ce383b2310
Shilong Liu <shilongliu@microsoft.com>
2023-07-26 09:59:17 +0000

    draft

--- sii_pr_review/pr_dump.sh
+++ sii_pr_review/pr_dump.sh
@@ -71,8 +71,8 @@ check_dump_file(){
     file=$1
     [ -f $file ] || return 11
     length=$(cat $file | jq length)
-    [[ $? != 0 ]] && return 12
-    echo $file,$length
+    [[ $? != 0 ]] || [[ $length == '' ]] && return 12
+    echo "        $file,$length"
     echo $length | grep -e '00$' -e '^1$' && echo Warning: $file,$length !!!!!!!!!! 1>&2 && return 13
     return 0
 }
@@ -85,13 +85,13 @@ do
     do
         repo=$(echo $org_repo | awk -F/ '{print$2}')
         org=$(echo $org_repo | awk -F/ '{print$1}')
-        echo "    " repo: $repo
+        echo "    repo: $repo"
         # try dump by year when bypass_year==n and no monthly dump files
         file_by_year=$year/$repo.$dump_type.json
         if [[ "$bypass_year" == n ]] && [ -z "$(ls $year/$repo.*.$dump_type.json 2>/dev/null)" ];then
             check_dump_file $file_by_year && continue
             rm -rf $file_by_year
-            eval $debug gh pr list -R $org_repo -L 10000 -s merged --json $keys -S "merged:$year-01-01..$year-12-31" | jq --indent 4 ".[] += {repo: \"$repo\", author: .[].author.login} | sort_by(.number)"  > $file_by_year
+            eval $debug gh pr list -R $org_repo -L 10000 -s merged --json $keys -S "merged:$year-01-01..$year-12-31" | jq --indent 4 ".[] += {repo: \"$repo\"} | sort_by(.number)"  > $file_by_year
             sleep $interval
             check_dump_file $file_by_year && continue
         fi
@@ -103,7 +103,7 @@ do
             start=$year-$month-01
             # if the last day is not correct, download will fail.
             end=$(date -d "$year/$month/1 + 1 month -1 day" "+%Y-%m-%d")
-            echo "        $start,$end"
+            echo "            $start,$end"
             check_dump_file $file_by_month && continue
             eval $debug gh pr list -R $org_repo -L 10000 -s merged --json $keys -S "merged:$start..$end" | jq --indent 4 ".[] += {repo: \"$repo\"}" > $file_by_month
             sleep $interval
@@ -119,9 +119,10 @@ do
             eval $debug gh pr list -R $org_repo -L 10000 -s merged --json $keys -S "merged:$year-$month-21..$end" | jq --indent 4 ".[] += {repo: \"$repo\"}" > $year/$repo.$month.c.$dump_type.json
             check_dump_file $year/$repo.$month.c.$dump_type.json || { echo "gh pr list -R $org_repo -L 10000 -s merged --json $keys -S\"merged:$year-$month-21..$end\""; exit 113; }
             sleep $interval
-            jq 'add | sort_by(.number)' --indent 4 $year/$repo.$month.a.$dump_type.json $year/$repo.$month.b.$dump_type.json $year/$repo.$month.c.$dump_type.json > $year/$repo.$month.$dump_type.json
+            jq -s 'add' --indent 4 $year/$repo.$month.a.$dump_type.json $year/$repo.$month.b.$dump_type.json $year/$repo.$month.c.$dump_type.json > $year/$repo.$month.$dump_type.json
+            rm $year/$repo.$month.a.$dump_type.json $year/$repo.$month.b.$dump_type.json $year/$repo.$month.c.$dump_type.json
         done    
-        jq 'add | sort_by(.number)' --indent 4 $year/$repo.*.$dump_type.json > $file_by_year
-        git add $file_by_year
+        jq -s 'add | sort_by(.number)' --indent 4 $year/$repo.*.$dump_type.json > $file_by_year
+        rm $year/$repo.*.$dump_type.json
     done
 done
